07675e7539439eb429b2f2b8b47d7b4275792567
FraserShimmins <shimmins.f@gmail.com>
2026-01-31 12:17:48 +0000

    Add a Column() to the 'body' of the MyApp in 'example_1'

--- example_1/lib/main.dart
+++ example_1/lib/main.dart
@@ -13,25 +13,42 @@ void main() {
 // Custom Widget that will be passed to 'MyApp()'
 //
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   // Build Method = Method which returns a widget, is called everytime flutter needs to rebuild the UI
   // - e.g. when your data changes
   // - Describes the UI
   @override
   Widget build(BuildContext context) {
-    
     // MaterialApp = Widget used as a root of the application
     //  - Allows us to configure themes and routes
     return MaterialApp(
       // Scaffold = A key layout component (container) that allows you to build screens with common UI elements
       //  - e.g. like an AppBar at the top of an application
       home: Scaffold(
-                      appBar: AppBar(
-                                      backgroundColor: Colors.green, 
-                                      title: const Text("Gerson Is tougher than the Roaring Knight!")
-                                    )
-                    )
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text("Gerson Is tougher than the Roaring Knight!"),
+        ),
+        // Body = Primary content of the scaffold
+        body: Column(
+            children: [ 
+              const Icon(Icons.egg),
+              const Icon(Icons.video_call),
+              const Icon(Icons.dark_mode),
+              // Container = A highly versatile widget that functions much like a multi-purpose box in your app's interface
+              //  - i.e. it's like 'div' in html
+              Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(border: Border.all(), color: Colors.purple),
+                child: const Text("[Insert Funny Joke Here]"),
+              ),
+            ],
+        ),
+      ),
     );
   }
 }
